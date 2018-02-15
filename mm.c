668d8f067fde94dbd6de6b9d32f6f888221124d8
Joseph Macdonald <joemacdonald21@gmail.com>
2018-02-15 10:54:15 -0500

    Update mm.c

--- mm.c
+++ mm.c
@@ -1,10 +1,31 @@
 
 /*
  * mm.c
+ * 
+ * This file is used for performing the c library functions malloc, free, 
+ * realloc, and calloc. Calloc as been provided. Author Joe Macdonald wrote 
+ * malloc, free, and realloc based loosely on the textbook. 
+ * 
+ * In init, we create a heap that we will manage by allocating or expanding 
+ * as needed. Initialize to size 16384 (this is arbitrary). We also initilize a
+ * global segregated list that is used to keep track of our free spaces and 
+ * quickly locate a compatible block that will work with the requested data size.
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
- *
+ * In malloc, we search our free list for a block that matches the adjusted size
+ * of the space we need. Each size is rounded up to the nearest multiple of 16, 
+ * then added with another 16 to provide space for the header and footer. To select
+ * a block to use, begin at the earliest spot that a compatible size could exist. 
+ * Traverse that list, and if no block is found, increment the index by one to 
+ * search the next list. We then call the place function to optimze the block size,
+ * which means splitting if necessary and adding/deleting from the list. Once the
+ * list is in order, return a pointer to the space just allocated.
+ *  
+ * 
+ * In realloc, we want to change the amount of space we use for a current block
+ * that is already allocated. We call our already-written functions of malloc
+ * and free. Malloc some free space, copy over old data, free old data pointer.
+ * Return a pointer to the space just allocated. 
+ * 
  */
 
 /* free block example
@@ -17,7 +38,22 @@
  *| header (8) | data (anysize)| footer (8) |
  * -----------------------------------------
  *
+ * example seg list:
+ *  ---  ->  ---
+ * | f |    | f |  NULL
+ *  ---  <-  ---
  *
+ *  ---  ->  ---  ->  ---  
+ * | f |    | f |    | f | ->   NULL
+ *  ---  <-  ---  <-  ---  
+ * 
+ * NULL
+ *
+ *  ---  ->  ---
+ * | f |    | f |  NULL
+ *  ---  <-  ---
+ *
+ * this would contiue for 12 spaces
  */
 
 
@@ -69,12 +105,11 @@
 
 void *seg_list[LISTSIZE];
 void *heap_ptr;
-int instructionsExecuted;
 
 //function prototypes for helper functions
 static void write_word(void *ptr, size_t val);
 static size_t read_word(void *ptr);
-size_t PACK(size_t size, int alloc);
+size_t combine(size_t size, int alloc);
 static void printBlock(void* ptr);
 size_t getAlloc(void* ptr);
 size_t getSize(void* ptr);
@@ -105,40 +140,40 @@ static size_t align(size_t x)
 }
 
 /*
- * Initialize: return false on error, true on success.
+ * allocate heap
+ * create prologue header
+ * create prologue footer
+ * create epilogue header
+ * create segregated list
+ * initial size of array
  */
 bool mm_init(void)
 {
-    //allocate heap
-    //create prologue header
-    //create prologue footer
-    //create epilogue header
-    //create bins/linked list objects
-    //initial size of array
-    instructionsExecuted = 0;
-    //printf("INIT\n");
-    int list;
-    heap_ptr = (size_t*) heap_ptr; //points to beginning of heap
+    int index;
+
+    //points to beginning of heap
+    heap_ptr = (size_t*) heap_ptr;
 
     //create our array of lists
     //initialize to NULL
-    for (list = 0; list < LISTSIZE; list ++){
-        seg_list[list] = NULL;
+    for (index = 0; index < LISTSIZE; index++){
+        seg_list[index] = NULL;
     }
-    if ((heap_ptr = mem_sbrk(ALIGNMENT*2)) == NULL)
+
+    //create space for prologue and epilogue, plus 8 byte padding 
+    if ((heap_ptr = mem_sbrk(ALIGNMENT*2)) == NULL) {
         return false;
-    write_word(heap_ptr, 0); //write 0 to beginning of heap
-    //offset by 8, create header and footer of prologue 
-    write_word(heap_ptr + HEADFOOT, PACK(2*HEADFOOT,1));
-    write_word(heap_ptr + (2*HEADFOOT), PACK(2*HEADFOOT,1)); 
-    //printBlock(heap_ptr + (2*HEADFOOT));
-    //mm_checkheap(1);
+    }
+
+    //write 0 to beginning of heap
+    //offset by 8, create header and footer of prologue
+    write_word(heap_ptr, 0); 
+    write_word(heap_ptr + HEADFOOT, combine(2*HEADFOOT,1));
+    write_word(heap_ptr + (2*HEADFOOT), combine(2*HEADFOOT,1)); 
     heap_ptr = extendHeap(INITHEAP);
-    //printf("heap_ptr: %p\n", heap_ptr);
-    //printBlock(heap_ptr);
-    //printf("END OF INIT\n");
-    instructionsExecuted++;
-    //printf("instructionsExecuted: %d\n", instructionsExecuted);
+
+    //only use for debugging
+    mm_checkheap(1);
     return true;
 }
 
@@ -147,10 +182,12 @@ bool mm_init(void)
  */
 void* malloc(size_t size)
 {
-    //printf("MALLOC of size %lu\n", size);
     void* ptr = NULL;
+    int index = 0;
     size_t asize;
-    if (size == 0) {    //bad request
+
+    //bad request
+    if (size == 0) {    
         return NULL;
     }
 
@@ -158,22 +195,44 @@ void* malloc(size_t size)
     if (size <= 16) {
         asize = MINBLOCKSIZE;     
     }
-    else { //add space for header and footer
+    else { 
+        //add space for header and footer
         asize = align(size + 16); //optimal alignment
     }
+
+
     //find the first bin the list that is compatible with our size
-    int index = 0;
-    //size_t searchSize = asize;
     //search for free block in segregated list
-    index = findList(asize);
+    index = placeList(asize);
+    
     //if there are no spots at that index, increase and try again
+    //for loop through first 10 elements of array and look for more compatible size
+    if (index != 11){
+        ptr = seg_list[index];
+        if (ptr != NULL){
+            for (int i = 0; i < 100; i++) {
+                if (ptr == NULL){
+                    break;
+                }
+                if (asize <= getSize(headerAddress(ptr))){
+                    //we've found a compatible block
+                    place(ptr, asize);
+                    return ptr;
+                }
+                ptr = nextListAddPointer(ptr);
+            }
+        }
+    }
+    index += 1;
+    ptr = NULL;
     while ((ptr == NULL) && (index < 12)) {
         ptr = seg_list[index];
         index++;
     }
+
     index--;
     if (index == 11){ //we must scan through that list
-        if (ptr == NULL) { //no values in 5 either 
+        if (ptr == NULL) { //no values in 11 either 
             ptr = extendHeap(max(asize, INITHEAP));
         }
         while ((asize > getSize(headerAddress(ptr)))) {
@@ -185,36 +244,34 @@ void* malloc(size_t size)
         }
         //scan through, determine if theres a spot
     }
-    else if( index == -1) {
-        printf("findList returned -1\n");
-    }
 
     //insert and divide block
     ptr = place(ptr, asize);
 
     //this is now the space of our newly allocated block
-    //printf("End of MALLOC\n");
-    instructionsExecuted++;
     return ptr;
 }
 
 /*
- * free
+ * free a space that has been allocated
+ * insert that space onto our seg list
+ * change allocation bit to 0
  */
 void free(void* ptr)
 {
-    //printf("FREE (%p of size %lu)\n", ptr, getSize(headerAddress(ptr)));
     size_t size = getSize(headerAddress(ptr));
-    write_word(headerAddress(ptr), PACK(size, 0));
-    write_word(footerAddress(ptr), PACK(size, 0));
+    write_word(headerAddress(ptr), combine(size, 0));
+    write_word(footerAddress(ptr), combine(size, 0));
     insertListElement(ptr, size);
     coalesce(ptr);
-    instructionsExecuted++;
     return;
 }
 
 /*
- * realloc
+ * take in old pointer and size to be changed. 
+ * malloc enough space for new objetct
+ * copy data from old spot into new spot
+ * free old pointer, return new
  */
 void* realloc(void* oldptr, size_t size)
 {
@@ -233,7 +290,6 @@ void* realloc(void* oldptr, size_t size)
     ptr = malloc(size);
     ptr = memcpy(ptr, oldptr, size);
     free(oldptr);
-
     return ptr;
 }
 
@@ -272,7 +328,8 @@ static bool aligned(const void* p)
 }
 
 /*
- * mm_checkheap
+ * commented out for submission. 
+ * checks both the heap and the free list
  */
 bool mm_checkheap(int lineno)
 {
@@ -301,7 +358,6 @@ bool mm_checkheap(int lineno)
 
         printBlock(heap_ptr);
         heap_ptr = heap_ptr + size;
-        //printf("looping...\n");
     }
     
     printf("CHECK LIST\n");
@@ -335,25 +391,30 @@ bool mm_checkheap(int lineno)
     return true;
 }
 
+
 //All helper functions
 //Macros would have made things a tad easier, but 
 //to be safe, they were not utilized
 
 
-//reading and writing word 
+/*
+ * read data at a pointer address
+ */
 static size_t read_word(void *ptr){
     //printf("READ WORD\n");
     //printf("ptr: %p\n", ptr);
     return *(size_t *)(ptr);
 }
-
+/*
+ * write data to a pointer address
+ */
 static void write_word(void *ptr, size_t val){
     //printf("WRITE WORD\n");
     *((size_t *)(ptr)) = val;
 }
 
 //combine the size with allocated or not for the header/footer
-size_t PACK(size_t size, int alloc){
+size_t combine(size_t size, int alloc){
     return (size | alloc);
 }
 
@@ -376,31 +437,34 @@ static void printBlock(void* ptr){
        hsize, (halloc ? 'a' : 'f'), 
        fsize, (falloc ? 'a' : 'f'));
 }
-
-//input header address, return size of block
+/*
+ * input header address, return size of block
+ * get address of ptr
+ * bitwise and with ~0x7
+ * return value should be size
+ */
 size_t getSize(void* ptr) {
-    //get address of ptr
-    //bitwise and with ~0x7
-    //return value should be size
-    //printf("GET SIZE\n");
-    //printf("ptr: %p\n", ptr);
     return (size_t)(read_word(ptr) & (~0xf));
 }
-//input header address, return allocated or not
+/*
+ * input header address, return allocated or not
+ * get address of ptr
+ * bitsise and with 0x1
+ * return value should be 1 or 0 for alloc or not
+ */
 size_t getAlloc(void* ptr) {
-    //get address of ptr
-    //bitsise and with 0x1
-    //return value should be 1 or 0 for alloc or not
     return (size_t)(read_word(ptr) & 0x1);
 }
 
-//input pointer to block, return address of header
+/*
+ * input pointer to block, return address of header
+ */
 size_t* headerAddress(void *ptr){
-    //printf("HEADER ADDRESS\n");
-    //printf("ptr: %p\n", ptr);
     return ((size_t *)(ptr) - 1);
 }
-//input pointer to block, return address of footer
+/*
+ * input pointer to block, return address of footer
+ */
 size_t* footerAddress(void *ptr){
     //printf("FOOTER ADDRESS\n");
     return (size_t *)((ptr) + getSize(headerAddress(ptr)) - (2*HEADFOOT));
@@ -416,40 +480,32 @@ size_t *prevBlock(void* ptr) {
     return (size_t *)((ptr) - getSize((ptr) - 16));
 }
 
-//we need more space, so extend the heap and create one big free block. 
+/*
+ * we need more space, so extend the heap and create one big free block. 
+ */
 static void *extendHeap(size_t size) {
-    //printf("EXTEND HEAP\n");
     size_t *p;
     size_t asize;
 
-    //make sure new block s 16 byte aligned
+    //make sure new block is 16 byte aligned
     asize = align(size);
-    //printf("asize: %lu\n", asize);
     if ((p = mem_sbrk(asize)) == NULL) {
         return NULL;
     }
-    //printf("asize: %lu\n", asize);
-    //printf("p after mem_sbrk: %p\n", p);
-    //printf("Extend Heap heap_ptr: %p\n", heap_ptr);
-    //p += 1; //offset to beginning of free data
-    write_word(headerAddress(p), PACK(asize, 0)); //free block header
-    write_word(footerAddress(p), PACK(asize, 0)); //free block footer
-    write_word(headerAddress(nextBlock(p)), PACK (0,1));
-    //printf("Header address of p: %p\n", headerAddress(p));
-    //printf("Footer address of p: %p\n", footerAddress(p));
-    //printf("Next Block of p: %p\n", nextBlock(p));
-    //mm_checkheap(1);
+    
+    write_word(headerAddress(p), combine(asize, 0)); //free block header
+    write_word(footerAddress(p), combine(asize, 0)); //free block footer
+    write_word(headerAddress(nextBlock(p)), combine(0,1));
     insertListElement(p, asize);
     return coalesce(p);
 }
-
-//combines free blocks 
-//four cases: no coalescing needed, coalesce right, 
-//coalesce left, coalesce both
-//we will need to keep track of these within the data structure
+/*
+ * combines free blocks 
+ * four cases: no coalescing needed, coalesce right, 
+ * coalesce left, coalesce both
+ * we will need to keep track of these within the data structure
+*/
 static void *coalesce(void *ptr) {
-    //printf("COALESCE\n");
-
     size_t prev = getAlloc(headerAddress(prevBlock(ptr)));
     size_t next = getAlloc(headerAddress(nextBlock(ptr)));
     size_t size = getSize(headerAddress(ptr));
@@ -463,8 +519,8 @@ static void *coalesce(void *ptr) {
         removeListElement(ptr);
         removeListElement(nextBlock(ptr));
         size += getSize(headerAddress(nextBlock(ptr)));
-        write_word(headerAddress(ptr), PACK(size, 0));
-        write_word(footerAddress(ptr), PACK(size, 0));
+        write_word(headerAddress(ptr), combine(size, 0));
+        write_word(footerAddress(ptr), combine(size, 0));
     }
 
     //coalesce left
@@ -472,8 +528,8 @@ static void *coalesce(void *ptr) {
         removeListElement(ptr);
         removeListElement(prevBlock(ptr));
         size += getSize(headerAddress(prevBlock(ptr)));
-        write_word(footerAddress(ptr), PACK(size,0));
-        write_word(headerAddress(prevBlock(ptr)), PACK(size, 0));
+        write_word(footerAddress(ptr), combine(size,0));
+        write_word(headerAddress(prevBlock(ptr)), combine(size, 0));
         ptr = prevBlock(ptr);
     }
 
@@ -484,8 +540,8 @@ static void *coalesce(void *ptr) {
         removeListElement(prevBlock(ptr));
         size += getSize(headerAddress(prevBlock(ptr)))
              + getSize(headerAddress(nextBlock(ptr)));
-        write_word(headerAddress(prevBlock(ptr)), PACK(size, 0));
-        write_word(footerAddress(nextBlock(ptr)), PACK(size, 0));
+        write_word(headerAddress(prevBlock(ptr)), combine(size, 0));
+        write_word(footerAddress(nextBlock(ptr)), combine(size, 0));
         ptr = prevBlock(ptr);
     }
 
@@ -494,7 +550,9 @@ static void *coalesce(void *ptr) {
     return ptr;
 }
 
-//value of pointer addresses on free list 
+/*
+ * value of pointer to address on seg list 
+ */
 size_t* prevListAddPointer(void* ptr) {
     return (*(size_t **)(ptr));
 }
@@ -502,19 +560,22 @@ size_t* nextListAddPointer(void* ptr) {
     return (*(size_t **)(nListPoint(ptr)));
 }
 
-//address of free blocks predecessor and successor entries
+/*
+ * address of free blocks predecessor and successor entries 
+ */
 size_t* pListPoint(void* ptr) {
-    //printf("P LIST POINT\n");
     return ((size_t *)(ptr));
 }
 size_t* nListPoint(void* ptr) {
-    //printf("N LIST POINT\n");
     return (size_t *)((ptr) + 8);
 }
 
-//placing data inside a free block
+/*
+ * places data inside a free block
+ * also calculates size of free space left in block,
+ * creates new block and adds it to the list if there is enough space
+ */
 void* place(void* ptr, size_t asize){
-    //printf("PLACE\n");
     size_t ptr_size = getSize(headerAddress(ptr));
     size_t leftover = ptr_size - asize;
     removeListElement(ptr);
@@ -523,23 +584,15 @@ void* place(void* ptr, size_t asize){
 
     //we can't split, we won't use a block this size
     if (leftover < 32) {
-        write_word(headerAddress(ptr), PACK(ptr_size, 1));
-        write_word(footerAddress(ptr), PACK(ptr_size, 1));
-    }
-    else if (asize >=100){
-        write_word(headerAddress(ptr), PACK(leftover, 0));
-        write_word(footerAddress(ptr), PACK(leftover, 0));
-        write_word(headerAddress(nextBlock(ptr)), PACK(asize, 1));
-        write_word(footerAddress(nextBlock(ptr)), PACK(asize, 1));
-        insertListElement(ptr, leftover);
-        return nextBlock(ptr);
+        write_word(headerAddress(ptr), combine(ptr_size, 1));
+        write_word(footerAddress(ptr), combine(ptr_size, 1));
     }
     //we want to split the block
     else {
-        write_word(headerAddress(ptr), PACK(asize, 1));
-        write_word(footerAddress(ptr), PACK(asize, 1));
-        write_word(headerAddress(nextBlock(ptr)), PACK(leftover, 0));
-        write_word(footerAddress(nextBlock(ptr)), PACK(leftover, 0));
+        write_word(headerAddress(ptr), combine(asize, 1));
+        write_word(footerAddress(ptr), combine(asize, 1));
+        write_word(headerAddress(nextBlock(ptr)), combine(leftover, 0));
+        write_word(footerAddress(nextBlock(ptr)), combine(leftover, 0));
         insertListElement(nextBlock(ptr), leftover);
     }
     return ptr;
@@ -580,43 +633,45 @@ static void removeListElement(void *ptr) {
             setPointer(pListPoint(nextListAddPointer(ptr)), prevListAddPointer(ptr));
         }
     }
-        
-
-    //printf("END OF REMOVE\n");
 }
 
+/*
+ * adds an item to the seg list by inserting to the front
+*/
 static void insertListElement(void *ptr, size_t size){
-    //printf("INSERT LIST ELEMENT\n");
     int index = 0;
     void *curr_head_ptr = NULL;
 
     //find list that fits with our size
     index = placeList(size);
-    //printf("index of block to be placed: %d\n", index);
     curr_head_ptr = seg_list[index];
+
     //always throw in the front of the list
     if (curr_head_ptr != NULL) { //no element in this spot
-        //printf("curr_head is NOT equal to NULL\n");
         setPointer(pListPoint(ptr), NULL);
         setPointer(nListPoint(ptr), curr_head_ptr);
         setPointer(pListPoint(curr_head_ptr), ptr);
         seg_list[index] = ptr;
     }
     else { //curr_head is equal to NULL
-        //printf("curr_head is equal to NULL\n");
         setPointer(nListPoint(ptr), NULL);
         setPointer(pListPoint(ptr), NULL);
         seg_list[index] = ptr;
     }
 }
 
+/*
+ * cast ptr to value, assign it into address
+ * used for when simple assignment won't work
+ */
 static void setPointer(void *address, void *ptr){
-    //printf("SET POINTER\n");
     *(size_t *)(address) = (size_t)(ptr);
 }
 
-//scan through list, find correct index of list
-//asize = is the block size we need
+/*
+ * scan through list, find correct index of list to take from
+ * asize = is the block size we need
+ */
 int findList(size_t asize){
     if (asize == 32) {
         return 0;
@@ -656,8 +711,10 @@ int findList(size_t asize){
     }
 }
 
-
-//determines which block the data will go in
+/*
+ * determines which block the data will go in
+ * asize = is the block size we need
+ */
 int placeList(size_t asize) {
     if (asize < 64) {
         return 0;
@@ -697,7 +754,11 @@ int placeList(size_t asize) {
     }
 }
 
-//get max of two sizes
+/*
+ * get max of two sizes (for extending heap)
+ */
 size_t max(size_t a, size_t b) {
     return (a > b) ? a : b;
 }
+
+//End of File
